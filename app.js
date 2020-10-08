276a2867c9c984074cd44d0531d2a754d56f183d
SnorlaxDevs <fahadkgla@gmail.com>
2020-10-08 21:46:04 +0530

    Add Work List

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date");
+
+const items = [];
+const workItems = [];
 
 const app = express();
+
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     // res.send("Hello World!");
-    var today = new Date();
+    const day = date.getDate();
+    res.render("list", {title: day, items: items });
+});
 
-    if (today.getDay() == 6 || today.getDay() == 0) {
-        res.send("Yay it's the weekend");
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    console.log(item);
+    if (req.body.list == "Work") {
+        workItems.push(item);
+        res.redirect("/work");
     } else {
-        res.send("Boo! I have to work!");
+        items.push(item);
+        res.redirect("/");
     }
+   
 });
 
 
+app.get("/work", (req, res) => {
+    res.render("list", {title: "Work List", items: workItems});
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
