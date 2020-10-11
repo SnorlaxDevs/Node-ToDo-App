3154732f0f589c8ca13f48353182db5dbd4a5363
SnorlaxDevs <fahadkgla@gmail.com>
2020-10-12 01:39:48 +0530

    Add Mongo Database to store and remove ToDo's

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date");
+const mongoose = require("mongoose");
 
-const items = [];
-const workItems = [];
+// const items = [];
+// const workItems = [];
 
 const app = express();
 
@@ -12,25 +13,84 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://mongoadmin:secret@localhost:27888/todolistDB?authSource=admin");
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your ToDoList!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+// Item.insertMany([item1, item2, item3], () => {
+//     if (err){
+//         console.log(err);
+//     } else {
+//         console.log("Successfully saved default items to DB");
+//     }
+// });
+
+const defaultItems = [item1, item2, item3];
+
 app.get("/", (req, res) => {
     // res.send("Hello World!");
     const day = date.getDate();
-    res.render("list", {title: day, items: items });
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully saved the defaultItems into DB.")
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", {title: day, items: foundItems});
+        }
+
+    })
 });
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
     console.log(item);
+    const newItemDocument = new Item({
+        name: item
+    });
     if (req.body.list == "Work") {
-        workItems.push(item);
+        // workItems.push(item);
         res.redirect("/work");
     } else {
-        items.push(item);
+        // items.push(item);
+        newItemDocument.save();
         res.redirect("/");
     }
-   
 });
 
+app.post("/delete", (req, res) => {
+    console.log(req.body.checkbox);
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Successfully Removed!");
+            res.redirect("/")
+        }
+    });
+});
 
 app.get("/work", (req, res) => {
     res.render("list", {title: "Work List", items: workItems});
